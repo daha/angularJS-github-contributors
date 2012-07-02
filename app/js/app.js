038b3f155a61b076b197da2004b663cf7fb10093
David Haglund <github2@davidhaglund.se>
2012-07-02 21:19:45 +0200

    reformating the code

--- app/js/app.js
+++ app/js/app.js
@@ -4,8 +4,19 @@
 angular.module('ghContrib', ['ghContrib.filters', 'ghContrib.services', 'ghContrib.directives']).
     config(['$routeProvider', function ($routeProvider) {
         'use strict';
-        $routeProvider.when('/', {templateUrl: 'partials/search.html', controller: UserCtrl});
-        $routeProvider.when('/github/:user/', {templateUrl: 'partials/user.html', controller: UserCtrl});
-        $routeProvider.when('/github/:user/:repo/', {templateUrl: 'partials/repo.html', controller: RepoCtrl});
-        $routeProvider.otherwise({redirectTo: '/'});
+        $routeProvider.when('/', {
+            templateUrl: 'partials/search.html',
+            controller: UserCtrl
+        });
+        $routeProvider.when('/github/:user/', {
+            templateUrl: 'partials/user.html',
+            controller: UserCtrl
+        });
+        $routeProvider.when('/github/:user/:repo/', {
+            templateUrl: 'partials/repo.html',
+            controller: RepoCtrl
+        });
+        $routeProvider.otherwise({
+            redirectTo: '/'
+        });
     }]);
