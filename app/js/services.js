d8aa8c5b0358c89db7fe15ace2eca8ac15dc8b9f
David Haglund <github2@davidhaglund.se>
2012-07-05 22:20:44 +0200

    Break up the githubResources code, to get a more realistic LOC comparison with the original.

--- app/js/services.js
+++ app/js/services.js
@@ -36,8 +36,17 @@
 angular.module('ghContrib.services', ['ngResource'], function ($provide) {
     'use strict';
     $provide.factory('githubResource', function ($resource) {
-        return $resource('https://api.github.com/:query/:user/:repo/:spec',
-            {'query': 'users', 'user': 'angular', 'repo': 'repos', 'spec': '', 'callback': 'JSON_CALLBACK', 'per_page': 100},
-            {'get': {'method': 'JSONP'}});
+        return $resource('https://api.github.com/:query/:user/:repo/:spec', {
+            'query': 'users',
+            'user': 'angular',
+            'repo': 'repos',
+            'spec': '',
+            'callback': 'JSON_CALLBACK',
+            'per_page': 100
+        }, {
+            'get': {
+                'method': 'JSONP'
+            }
+        });
     });
 });
