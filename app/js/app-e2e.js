55708b7e9280b1a0a770a40ef60920573342d749
David Haglund <github2@davidhaglund.se>
2012-07-09 22:09:47 +0200

    Starting to implement integration tests

--- app/js/app-e2e.js
+++ app/js/app-e2e.js
@@ -1,9 +1,9 @@
 /*globals angular,githubResponses */
 
-angular.module('ghContribE2E', ['ghContrib', 'ngMockE2E']).
-    run(function ($httpBackend) {
+angular.module('ghContribE2E', ['ghContrib', 'ngMockE2E'])
+    .run(function ($httpBackend) {
         'use strict';
-        $httpBackend.whenGET('https://api.github.com/').
-            respond(githubResponses.empty);
+        $httpBackend.whenGET('https://api.github.com/')
+            .respond(githubResponses.empty);
         $httpBackend.whenGET(/^partials\/[\s]*/).passThrough();
     });
--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -1,44 +1,33 @@
-/*globals describe,beforeEach,browser,it,expect,element */
+/*globals describe,beforeEach,browser,it,expect,element,toMatch,input */
 
 /* http://docs.angularjs.org/guide/dev_guide.e2e-testing */
 
 describe('gh contrib', function () {
     'use strict';
 
-    // beforeEach(function () {
-    //     browser().navigateTo('../../app/index.html');
-    // });
+    describe('GitHub Contributors with real data', function () {
+        beforeEach(function () {
+            browser().navigateTo('../../app/index.html');
+        });
 
+        it('should navigater to / when location hash is empty', function () {
+            expect(browser().location().url()).toBe("/");
+        });
 
-    // it('should automatically redirect to /view1 when location hash/fragment is empty', function () {
-    //     expect(browser().location().url()).toBe("/view1");
-    // });
+        it('should redirect invalid location hashed to /', function () {
+            browser().navigateTo('#/invalid-hash');
+            expect(browser().location().url()).toBe("/");
+        });
 
-    // describe('view1', function () {
+        it('should have the user model', function () {
+            input('user').enter('foo');
+        });
+    });
 
-    //     beforeEach(function () {
-    //         browser().navigateTo('#/view1');
-    //     });
+    describe('GitHub Contributors with mocked data', function () {
+        beforeEach(function () {
+            browser().navigateTo('../../app/index-e2e.html');
+        });
+    });
 
-
-    //     it('should render view1 when user navigates to /view1', function () {
-    //         expect(element('[ng-view] p:first').text()).
-    //             toMatch(/partial for view 1/);
-    //     });
-
-    // });
-
-    // describe('view2', function () {
-
-    //     beforeEach(function () {
-    //         browser().navigateTo('#/view2');
-    //     });
-
-
-    //     it('should render view2 when user navigates to /view2', function () {
-    //         expect(element('[ng-view] p:first').text()).
-    //             toMatch(/partial for view 2/);
-    //     });
-
-    // });
 });
